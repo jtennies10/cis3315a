9940d442ca34b21f9bc5653279ae2f49b6967fb3
Joshua Tennies <jdtennies10@gmail.com>
2017-02-21 09:07:57 -0500

    Inheritance and Polymorphism Example

---
+++ src/week4/C11PetDog.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4;
+
+/**
+ *
+ * @author jten10
+ */
+public class C11PetDog extends C11Pet {
+
+    @Override
+    public String speak() {
+        return "ruff";
+    }
+    
+}
