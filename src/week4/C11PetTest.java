fbbec7a07d7aab01e0320e6de1a87a4397c42c83
Joshua Tennies <jdtennies10@gmail.com>
2017-02-21 09:20:22 -0500

    Inheritance and Polymorphism Example

--- src/week4/C11PetTest.java
+++ src/week4/C11PetTest.java
@@ -12,8 +12,12 @@ package week4;
 public class C11PetTest {
     public static void main(String[] args) {
         C11Pet pc1 = new C11PetCat();
+        pc1.setPetName("Mittens");
         C11Pet pd1 = new C11PetDog();
+        pd1.setPetName("Fido");
         System.out.println("The cat says " + pc1.speak());
         System.out.println("The dog says " + pd1.speak());
+        System.out.println(pc1);
+        System.out.println(pd1);
     }
 }
